f1c08ec1271dcc275a23bd16228faec2408d7750
Sadegh <sadegh.tahani@gmail.com>
2025-12-30 09:58:21 +0300

    Initial commit: Project setup with CMP, Room, and Koin

---
+++ composeApp/src/commonMain/kotlin/com/sadeghtahani/notebox/Platform.kt
@@ -0,0 +1,7 @@
+package com.sadeghtahani.notebox
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
---
+++ composeApp/src/iosMain/kotlin/com/sadeghtahani/notebox/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.sadeghtahani.notebox
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
